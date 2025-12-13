fea89f3abca74fe8ffcf3f022d980e3947d3f2f5
Asif2501 <dhanushrajavel24@gmail.com>
2025-12-13 16:13:02 +0530

    bugs fixing

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
+        unoptimized: true,
     domains: ['images.pexels.com','res.cloudinary.com','images.pexels.com','cdn-icons-png.flaticon.com','i.pinimg.com',], // Add your allowed image domain here
   },
   /* config options here */
